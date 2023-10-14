281b8c3b457bb44c2c62d9ca928b510a6d3f854a
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2023-10-14 15:57:38 +0530

    added contact class

---
+++ design solutions/car_rental_system/src/main/java/com/abhinesh/rental/model/account/Contact.java
@@ -0,0 +1,11 @@
+package design solutions.car_rental_system.src.main.java.com.abhinesh.rental.model.account;
+
+
+@Getter
+@Setter
+public class Contact {
+    private String phone;
+    private String email;
+    private Address address;
+    private PersonalInfo personalInfo;
+}
