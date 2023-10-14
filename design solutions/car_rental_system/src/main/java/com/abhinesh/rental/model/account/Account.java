fdcbbe514896ea6ba7bb68db760d2e69017d07ab
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2023-10-14 15:43:15 +0530

    first commit

---
+++ design solutions/car_rental_system/src/main/java/com/abhinesh/rental/model/account/Account.java
@@ -0,0 +1,14 @@
+package com.abhinesh.rental.model.account;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public abstract class Account {
+    private String id;
+    private String email;
+    private String userName;
+    private String password;
+    private LocalDateTime lastAccessed;
+    private Contact contact;
+}
